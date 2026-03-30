da82c3f341a17d6d375815c305b70b952f5bb13a
afx1337 <101255001+afx1337@users.noreply.github.com>
2026-03-30 10:16:56 +0200

    Update afxGlmPerm.m

--- scripts/afxGlmPerm.m
+++ scripts/afxGlmPerm.m
@@ -182,18 +182,22 @@ function lastLen = printProgress(barWidth, pct, elapsedMin, lastLen)
     outStr = sprintf('  %3d %% %s', pct, barStr);
     
     if pct > 0 && pct < 100
-        totalEst   = elapsedMin / (pct/100);
-        totalSeconds = round(max(0, totalEst - elapsedMin)*60);
-        mm = floor(totalSeconds / 60);
-        ss = mod(totalSeconds, 60);
-        outStr = [outStr, sprintf(' (%d:%02d min left)', mm, ss)];
+        totalEst = elapsedMin / (pct/100);
+        remSecs = max(0, totalEst - elapsedMin)*60;
+        outStr = [outStr, sprintf(' (%s min left)', sec2mmss(remSecs))];
     end
 
     fprintf([backStr '%s'], outStr);
     lastLen = length(outStr);
 
     if pct == 100
-        fprintf(' done.\n');
+        fprintf(' done (%s min).\n',sec2mmss(elapsedMin*60));
         lastLen = 0;
     end
+end
+
+function str = sec2mmss(sec)
+    mm = floor(sec / 60);
+    ss = mod(sec, 60);
+    str = sprintf('%d:%02d', mm, round(ss));
 end
